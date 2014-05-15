b682229da9bf74ede3da934afbe7999bc4a571c5
Mariano Ravinale <mravinale@gmail.com>
2014-05-14 21:46:47 -0300

    started editing

--- public/modules/panels/controllers/panel_edit.js
+++ public/modules/panels/controllers/panel_edit.js
@@ -1,8 +1,17 @@
 'use strict';
 //http://tympanus.net/Tutorials/CSS3ButtonSwitches/index.html
 angular.module('meanp')
-    .controller('PanelEditCtrl', function ($scope, socket) {
+    .controller('PanelEditCtrl', function ($scope, $routeParams) {
 
-
+        panelService.getById($routeParams.id)
+            .success(function (response, status, headers, config) {
+                $scope.panel = response.data 
+            })
+            .error(function(response, status, headers, config) {
+                angular.forEach(response.errors, function(error, field) {
+                    form[field].$setValidity('mongoose', false);
+                    $scope.errors[field] = error.type;
+                });
+            });
 
     });
