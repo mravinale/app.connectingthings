18eff90faca6e740bd54cbba01564d013649661e
Mariano Ravinale <mravinale@gmail.com>
2014-10-16 19:00:42 -0300

    changed name

--- public/assets_new/js/controllers.js
+++ public/assets_new/js/controllers.js
@@ -12,7 +12,7 @@ angular.module('app.controllers', ['pascalprecht.translate', 'ngCookies'])
 
     // config
     $scope.app = {
-      name: 'Connecthings',
+      name: 'ConnectingThings',
       version: '1.0.3',
       // for chart colors
       color: {
