56e6d566728ba8cb0473352744d9b273eabe7453
Mariano Ravinale <mravinale@gmail.com>
2015-03-21 19:00:35 -0300

    initial commit

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var express = require('express'),
     mongoStore = require('connect-mongo')(express),
     config = require('./api/config/config'),
     mongoose = require('mongoose'),
-    moment = require('moment');
+    moment = require('moment'),
+   nodeRSA = require('node-rsa');
 
 
 var app = express();
@@ -96,12 +97,37 @@ var Message = mongoose.model('Message');
 // Start socket conection
 var ponteServer = ponte(opts);
 var lastValue = { topic:null, message:null }
+
+var tryParseJson = function(str) {
+    try {
+        return JSON.parse(str);
+    } catch (ex) {
+        return null;
+    }
+};
+
+var key = new nodeRSA(
+        '-----BEGIN RSA PRIVATE KEY-----\n'+
+        'MIIBOQIBAAJAVY6quuzCwyOWzymJ7C4zXjeV/232wt2ZgJZ1kHzjI73wnhQ3WQcL\n'+
+        'DFCSoi2lPUW8/zspk0qWvPdtp6Jg5Lu7hwIDAQABAkBEws9mQahZ6r1mq2zEm3D/\n'+
+        'VM9BpV//xtd6p/G+eRCYBT2qshGx42ucdgZCYJptFoW+HEx/jtzWe74yK6jGIkWJ\n'+
+        'AiEAoNAMsPqwWwTyjDZCo9iKvfIQvd3MWnmtFmjiHoPtjx0CIQCIMypAEEkZuQUi\n'+
+        'pMoreJrOlLJWdc0bfhzNAJjxsTv/8wIgQG0ZqI3GubBxu9rBOAM5EoA4VNjXVigJ\n'+
+        'QEEk1jTkp8ECIQCHhsoq90mWM/p9L5cQzLDWkTYoPI49Ji+Iemi2T5MRqwIgQl07\n'+
+        'Es+KCn25OKXR/FJ5fu6A6A+MptABL3r8SEjlpLc=\n'+
+        '-----END RSA PRIVATE KEY-----');
+
+
 ponteServer.on("updated", function(resource, buffer) {
+
     var message = { topic: resource, message: buffer.toString() };
 
-    io.sockets.emit(message.topic, message.message);
+    var decrypted = key.decrypt(message.key, 'utf8');
 
+
+    io.sockets.emit(message.topic, message.message);
     if(lastValue.topic == message.topic && lastValue.message ==  message.message) return; //add configuration
+
     //TODO: we should validate format
     var newMessage = new Message(message);
     newMessage.save(function(err, item) {
