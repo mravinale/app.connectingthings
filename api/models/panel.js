f6d66753ca179595fab16b0a6e432e1269565cfc
Mariano Ravinale <mravinale@gmail.com>
2015-06-10 11:20:51 -0300

    needs fixing in panels update

--- api/models/panel.js
+++ api/models/panel.js
@@ -7,9 +7,9 @@ var mongoose = require('mongoose'),
 var PanelSchema = new Schema({
   _id: { type: String },
   name: { type: String, required: true },
-  device: { type: String, ref: 'Device' },
-  camera: { type: String, ref: 'Camera' },
-  sensor: { type: String, ref: 'Sensor' },
+  device: { type: String, ref: 'Device', default: null },
+  camera: { type: String, ref: 'Camera', default: null },
+  sensor: { type: String, ref: 'Sensor', default: null },
   type: { type: String, required: true  },
   size: { type: String, required: true  },
   owner: { type: String, ref: 'User'},
@@ -23,17 +23,21 @@ PanelSchema.pre('save', function (next) {
     next();
 });
 
-
+//For update use better the controller
+//https://github.com/winstonjs/winston-mongodb
 PanelSchema.path('camera').validate(function(camera){
-     if(this.type === "camera" && !camera) return false;
+    if(!this && !camera) return false; //edit
+    if(this && this.type === "camera" && !camera) return false;
 }, 'Path `camera` is required.');
 
 PanelSchema.path('device').validate(function(device){
-     if(this.type === "camera" && !device) return true;
+    if(!this && !device) return false; //edit
+    if(this && this.type !== "camera" && !device) return false;
 }, 'Path `device` is required.');
 
 PanelSchema.path('sensor').validate(function(sensor){
-     if(this.type === "camera" && !sensor) return true;
+    if(!this && !sensor) return false; //edit
+    if(this && this.type !== "camera" && !sensor) return false;
 }, 'Path `sensor` is required.');
 
 mongoose.model('Panel', PanelSchema);
