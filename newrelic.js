dd96bc2a6fc4972070156fdd5c03cdd22d0f9d28
Mariano Ravinale <mravinale@gmail.com>
2015-07-11 13:35:39 -0300

    added new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['ConnectingThings App'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '2ee1720a0a13b11e1c8f46389d11af1ab2f3be29',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
