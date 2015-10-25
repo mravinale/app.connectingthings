fb9c458a678b85b5c318eb4e09c291e71f490340
Mariano Ravinale <mravinale@gmail.com>
2015-10-24 21:02:12 -0300

    minor fix

--- api/config/config.js
+++ api/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	port: process.env.PORT || 3000,
     db:{
          default: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.LOCAL_URL || 'mongodb://admin:system2610@107.170.11.168:27017/test',
-         mqtt: process.env.MONGOLAB_MQTT || process.env.MONGOHQ_MQTT || process.env.LOCAL_MQTT || 'mongodb://admin:system2610@107.170.11.168:27017mqtt'
+         mqtt: process.env.MONGOLAB_MQTT || process.env.MONGOHQ_MQTT || process.env.LOCAL_MQTT || 'mongodb://admin:system2610@107.170.11.168:27017/mqtt'
     },
     masterKey:"wd40"
 }
