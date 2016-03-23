ed314831a16eb15b3dad8ac5bd11c414c355b3d4
Mariano Ravinale <mravinale@gmail.com>
2016-03-22 23:25:01 -0300

    update mongodb to docker replica

--- api/config/config.js
+++ api/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   port: process.env.PORT || 3000,
   db:{
-    default: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.LOCAL_URL || 'mongodb://admin:system2610@app.connectingthings.io/test',
-    mqtt: process.env.MONGOLAB_MQTT || process.env.MONGOHQ_MQTT || process.env.LOCAL_MQTT || 'mongodb://admin:system2610@app.connectingthings.io/mqtt'
+    default: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.LOCAL_URL || 'mongodb://10.7.0.3:27017,10.7.0.5:27017/main',
+    mqtt: process.env.MONGOLAB_MQTT || process.env.MONGOHQ_MQTT || process.env.LOCAL_MQTT || 'mongodb://10.7.0.3:27017,10.7.0.5:27017/mqtt',
   },
   masterKey:"wd40"
 };
