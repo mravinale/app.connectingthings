00032a3bf597585b0d9320bbd86c05bd41444114
Mariano Ravinale <mravinale@gmail.com>
2016-03-24 11:12:01 -0300

    fix baformed database

--- api/config/config.js
+++ api/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   port: process.env.PORT || 3000,
   db:{
-    default: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.LOCAL_URL || 'admin:system2610@app.connectingthings.io/test',
-    mqtt: process.env.MONGOLAB_MQTT || process.env.MONGOHQ_MQTT || process.env.LOCAL_MQTT || 'admin:system2610@app.connectingthings.io/mqtt',
+    default: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.LOCAL_URL || 'mongodb://admin:system2610@app.connectingthings.io/test',
+    mqtt: process.env.MONGOLAB_MQTT || process.env.MONGOHQ_MQTT || process.env.LOCAL_MQTT || 'mongodb://admin:system2610@app.connectingthings.io/mqtt',
   },
   masterKey:"wd40"
 };
