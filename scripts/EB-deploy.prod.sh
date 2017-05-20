6cc0ad4bf4d874e3db1046a19c9f83afbccb96c5
Mariano Ravinale <mravinale@gmail.com>
2017-05-20 18:04:14 -0300

    minor fixes

--- scripts/EB-deploy.prod.sh
+++ scripts/EB-deploy.prod.sh
@@ -4,8 +4,8 @@ SHA1=$1
 EB_BUCKET=elasticbeanstalk-us-east-1-085850360071
 
 # Create new Elastic Beanstalk version
-DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json > $DOCKERRUN_FILE
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.prod.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.prod.json > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region us-east-1
 aws elasticbeanstalk create-application-version --application-name connectingthings \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE --region us-east-1
