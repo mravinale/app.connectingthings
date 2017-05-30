732639b6112f7ae10482be4a21780d7b080269a0
Mariano Ravinale <mravinale@gmail.com>
2017-05-30 19:56:00 -0300

    updated memory reservation

--- server.js
+++ server.js
@@ -74,7 +74,11 @@ app.configure( function(){
     app.use(express.static(__dirname + '/dist'));
   }
 });
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  next();
+});
 app.use(express.logger('dev'));
 app.use(express.cookieParser('MEANP'));
 app.use(express.cookieSession());
